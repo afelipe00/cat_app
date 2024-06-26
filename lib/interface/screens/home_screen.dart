779166f372bc928f7c2312c651235570e4e69c25
Felipe Mac <a.felipediazr00@gmail.com>
2024-06-26 13:46:40 -0500

    feat: :sparkles: primer version de appbar en el home

--- lib/interface/screens/home_screen.dart
+++ lib/interface/screens/home_screen.dart
@@ -8,19 +8,91 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final ThemeProvider themeProvider = Provider.of<ThemeProvider>(context);
+    double expandedHeight = 180.0; // TODO reviar si debe estar acá
+    double collapsedHeight = 60.0;
+    final ThemeData theme = Theme.of(context);
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Catbreeds'),
-      ),
-      body: Center(
-        child: Text('Home Screen'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          themeProvider.toggleTheme();
+      // appBar: AppBar(
+      //   title: const Text('Catbreeds'),
+      //   centerTitle: true,
+      //   bottom: PreferredSize(
+      //     preferredSize: Size.fromHeight(50.0),
+      //     child: Container(
+      //       padding: EdgeInsets.all(10.0),
+      //       child: TextField(
+      //         decoration: InputDecoration(
+      //           hintText: 'Search',
+      //           prefixIcon: Icon(Icons.search),
+      //           border: OutlineInputBorder(
+      //             borderRadius: BorderRadius.circular(12.0),
+      //           ),
+      //         ),
+      //       ),
+      //     ),
+      //   ),
+      // ),
+      body: NestedScrollView(
+        headerSliverBuilder: (context, innerBoxIsScrolled) {
+          return [
+            SliverAppBar(
+              title: Text(
+                'Catbreeds',
+                style: theme.textTheme.titleLarge!.copyWith(
+                  color: Colors.white,
+                ),
+              ),
+              centerTitle: true,
+              floating: true,
+              snap: true,
+              pinned: true,
+              expandedHeight: expandedHeight,
+              collapsedHeight: collapsedHeight,
+              flexibleSpace: FlexibleSpaceBar(
+                background: Container(
+                  color: Colors.black,
+                  child: Opacity(
+                    opacity: 0.5,
+                    child: Image.asset(
+                      'assets/images/cat1_bg.jpg',
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+              ),
+              bottom: PreferredSize(
+                preferredSize: const Size.fromHeight(50.0),
+                child: Container(
+                  padding: const EdgeInsets.all(10.0),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: 'Search',
+                      prefixIcon: const Icon(Icons.search),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12.0),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              actions: [
+                IconButton(
+                  icon: Icon(
+                    themeProvider.themePreference == ThemePreference.light ? Icons.light_mode : Icons.dark_mode,
+                  ),
+                  onPressed: () {
+                    themeProvider.toggleTheme();
+                  },
+                ),
+              ],
+            )
+          ];
         },
-        child: Icon(Icons.add),
+        body: Container(
+          child: const Center(
+            child: Text('Home Screen'),
+          ),
+        ),
       ),
     );
   }
