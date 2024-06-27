c5a9b03abdda04b09f808dba80c9df2a50c1cdad
Felipe Mac <a.felipediazr00@gmail.com>
2024-06-27 11:05:22 -0500

    refactor: :recycle: se modifica la vista de profile

--- lib/interface/screens/profile_screen.dart
+++ lib/interface/screens/profile_screen.dart
@@ -1,10 +1,13 @@
+import 'package:cat_app/resources/providers/theme_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final ThemeProvider themeProvider = Provider.of<ThemeProvider>(context);
     final size = MediaQuery.of(context).size;
     final colors = Theme.of(context).colorScheme;
     return Scaffold(
@@ -14,69 +17,113 @@ class ProfileScreen extends StatelessWidget {
             height: size.height * 0.4,
             width: size.width,
             decoration: BoxDecoration(
-              color: colors.primary,
+              color: colors.primaryContainer,
               borderRadius: const BorderRadius.only(
                 bottomLeft: Radius.circular(42),
                 bottomRight: Radius.circular(42),
               ),
             ),
-          ),
-          Expanded(
-            child: ListView.builder(
-              scrollDirection: Axis.horizontal,
-              itemCount: 5,
-              itemBuilder: (context, index) {
-                return Card(
-                  child: Container(
-                    height: 100,
-                    width: 100,
-                    color: colors.primary,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const SizedBox(height: 28.0),
+                Container(
+                  padding: const EdgeInsets.all(2.0),
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: colors.surface,
                   ),
-                );
-              },
+                  child: CircleAvatar(
+                    radius: 50,
+                    backgroundColor: colors.primary,
+                    backgroundImage: const NetworkImage("https://picsum.photos/200/300.jpg"),
+                  ),
+                ),
+                const SizedBox(height: 10),
+                Text(
+                  'Username',
+                  style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                        color: colors.onPrimaryContainer,
+                        fontWeight: FontWeight.bold,
+                      ),
+                ),
+                Text(
+                  'profile description here',
+                  style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                        color: colors.onPrimaryContainer,
+                      ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    IconButton(
+                      icon: Icon(
+                        Icons.edit,
+                        color: colors.onPrimaryContainer,
+                      ),
+                      onPressed: () {},
+                    ),
+                    IconButton(
+                      icon: Icon(
+                        Icons.settings,
+                        color: colors.onPrimaryContainer,
+                      ),
+                      onPressed: () {},
+                    ),
+                  ],
+                )
+              ],
             ),
           ),
           Expanded(
-            child: ListView(
-              children: [
-                ExpansionTile(
-                  title: const Text("General"),
-                  leading: const Icon(Icons.person),
-                  controlAffinity: ListTileControlAffinity.leading,
+            child: Theme(
+              data: Theme.of(context).copyWith(dividerColor: Colors.transparent),
+              child: MediaQuery.removePadding(
+                context: context,
+                removeTop: true,
+                child: ListView(
+                  shrinkWrap: true,
                   children: [
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 12.0, vertical: 2.0),
-                      child: Card(
-                        shadowColor: Colors.transparent,
-                        elevation: 0.5,
-                        child: ListTile(
-                          leading: Icon(
-                            Icons.dark_mode_outlined,
-                            color: colors.primary,
-                          ),
-                          title: Text(
-                            'Modo oscuro',
-                            style: TextStyle(
-                              fontFamily: "Boston",
-                              fontWeight: FontWeight.normal,
-                              color: colors.primary,
-                              fontSize: 14.0,
+                    ExpansionTile(
+                      title: const Text("General"),
+                      leading: const Icon(Icons.person),
+                      controlAffinity: ListTileControlAffinity.leading,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 12.0, vertical: 2.0),
+                          child: Card(
+                            shadowColor: Colors.transparent,
+                            elevation: 0.5,
+                            child: ListTile(
+                              leading: Icon(
+                                themeProvider.themePreference == ThemePreference.light
+                                    ? Icons.dark_mode
+                                    : Icons.light_mode,
+                                color: colors.primary,
+                              ),
+                              title: Text(
+                                'Modo oscuro',
+                                style: TextStyle(
+                                  fontFamily: "Boston",
+                                  fontWeight: FontWeight.normal,
+                                  color: colors.primary,
+                                  fontSize: 14.0,
+                                ),
+                              ),
+                              trailing: Switch(
+                                value: themeProvider.themePreference == ThemePreference.light ? false : true,
+                                onChanged: (value) {
+                                  themeProvider.toggleTheme();
+                                },
+                              ),
                             ),
                           ),
-                          trailing: Switch(
-                            value: false,
-                            onChanged: (value) {},
-                            activeColor: colors.primary,
-                            activeTrackColor: colors.primary.withOpacity(0.5),
-                            inactiveThumbColor: colors.primary.withOpacity(0.5),
-                            inactiveTrackColor: colors.primary.withOpacity(0.5),
-                          ),
-                        ),
-                      ),
-                    )
+                        )
+                      ],
+                    ),
                   ],
                 ),
-              ],
+              ),
             ),
           )
         ],
