43455e791b7046a5e2b880d286d018365a8eb175
Felipe Mac <a.felipediazr00@gmail.com>
2024-06-27 13:06:22 -0500

    feat: :sparkles: se agrega permiso de internet para android

--- lib/interface/screens/navigator_screen.dart
+++ lib/interface/screens/navigator_screen.dart
@@ -16,35 +16,38 @@ class _NavigatorScreenState extends State<NavigatorScreen> {
   @override
   Widget build(BuildContext context) {
     final colors = Theme.of(context).colorScheme;
-    return Scaffold(
-      body: widget.navigationShell,
-      bottomNavigationBar: NavigationBar(
-        selectedIndex: widget.navigationShell.currentIndex,
-        onDestinationSelected: (index) {
-          setState(() {
-            _selectedIndex = index;
-          });
-          widget.navigationShell.goBranch(
-            index,
-            initialLocation: index == widget.navigationShell.currentIndex,
-          );
-        },
-        indicatorColor: colors.primaryContainer,
-        labelBehavior: NavigationDestinationLabelBehavior.onlyShowSelected,
-        destinations: [
-          const NavigationDestination(
-            icon: Icon(Icons.screen_rotation_alt_rounded),
-            label: "Random",
-          ),
-          NavigationDestination(
-            icon: Icon(_selectedIndex == 1 ? Icons.home : Icons.home_outlined),
-            label: "Home",
-          ),
-          NavigationDestination(
-            icon: Icon(_selectedIndex == 2 ? Icons.person_rounded : Icons.person_outline_rounded),
-            label: "Profile",
-          ),
-        ],
+    return PopScope(
+      canPop: false,
+      child: Scaffold(
+        body: widget.navigationShell,
+        bottomNavigationBar: NavigationBar(
+          selectedIndex: widget.navigationShell.currentIndex,
+          onDestinationSelected: (index) {
+            setState(() {
+              _selectedIndex = index;
+            });
+            widget.navigationShell.goBranch(
+              index,
+              initialLocation: index == widget.navigationShell.currentIndex,
+            );
+          },
+          indicatorColor: colors.primaryContainer,
+          labelBehavior: NavigationDestinationLabelBehavior.onlyShowSelected,
+          destinations: [
+            const NavigationDestination(
+              icon: Icon(Icons.screen_rotation_alt_rounded),
+              label: "Random",
+            ),
+            NavigationDestination(
+              icon: Icon(_selectedIndex == 1 ? Icons.home : Icons.home_outlined),
+              label: "Home",
+            ),
+            NavigationDestination(
+              icon: Icon(_selectedIndex == 2 ? Icons.person_rounded : Icons.person_outline_rounded),
+              label: "Profile",
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/interface/screens/random_screen.dart
+++ lib/interface/screens/random_screen.dart
@@ -16,6 +16,7 @@ class RandomScreen extends StatefulWidget {
 class _RandomScreenState extends State<RandomScreen> {
   late CatBloc catBloc;
   bool isLoaded = true;
+  bool exitApp = false;
   Cat? cat;
 
   @override
@@ -32,54 +33,57 @@ class _RandomScreenState extends State<RandomScreen> {
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Random'),
-      ),
-      body: SafeArea(
-        child: Center(
-          child: BlocListener<CatBloc, CatState>(
-            listener: (context, state) {
-              if (state is CatLoaded) {
-                setState(() {
-                  cat = state.cats.first;
-                  isLoaded = false;
-                });
-              }
-            },
-            child: Skeletonizer(
-              enabled: isLoaded,
-              child: ParallaxCard(
-                height: size.height * 0.6,
-                width: size.width * 0.9,
-                child: Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(24),
-                    image: cat != null
-                        ? DecorationImage(
-                            image: NetworkImage(cat!.url!),
-                            fit: BoxFit.cover,
-                          )
-                        : const DecorationImage(
-                            image: AssetImage('assets/images/cat1_bg.jpg'),
-                            fit: BoxFit.cover,
-                          ),
+    return PopScope(
+      canPop: false,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Random'),
+        ),
+        body: SafeArea(
+          child: Center(
+            child: BlocListener<CatBloc, CatState>(
+              listener: (context, state) {
+                if (state is CatLoaded) {
+                  setState(() {
+                    cat = state.cats.first;
+                    isLoaded = false;
+                  });
+                }
+              },
+              child: Skeletonizer(
+                enabled: isLoaded,
+                child: ParallaxCard(
+                  height: size.height * 0.6,
+                  width: size.width * 0.9,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(24),
+                      image: cat != null
+                          ? DecorationImage(
+                              image: NetworkImage(cat!.url!),
+                              fit: BoxFit.cover,
+                            )
+                          : const DecorationImage(
+                              image: AssetImage('assets/images/cat1_bg.jpg'),
+                              fit: BoxFit.cover,
+                            ),
+                    ),
                   ),
                 ),
               ),
             ),
           ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          catBloc.add(const FetchCatEvent(
-            order: GetCatMode.random,
-            page: 0,
-            limit: 1,
-          ));
-        },
-        child: const Icon(Icons.replay_rounded),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            catBloc.add(const FetchCatEvent(
+              order: GetCatMode.random,
+              page: 0,
+              limit: 1,
+            ));
+          },
+          child: const Icon(Icons.replay_rounded),
+        ),
       ),
     );
   }
